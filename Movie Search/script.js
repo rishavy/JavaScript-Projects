cd83225bebab0f214709c0ae807f11020974eda3
RISHAV YADAV <128891361+rishavy@users.noreply.github.com>
2024-03-25 23:48:41 +0530

    movie search

---
+++ Movie Search/script.js
@@ -0,0 +1,75 @@
+const API_KEY = 'e48a43b1';
+const SEARCH_DELAY = 300;
+
+let debounceTimer;
+
+const searchMovies = async (query) => {
+    try {
+    const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`);
+    const data = await response.json();
+    return data.Search || [];
+    } catch (error) {
+    console.error('Error fetching movies:', error);
+    return [];
+    }
+};
+
+const debounceSearch = (func, delay) => {
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(func, delay);
+};
+
+const renderMovies = (movies) => {
+    const movieList = document.getElementById('movieList');
+    movieList.innerHTML = '';
+    movies.forEach(movie => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('movie-item');
+    const poster = movie.Poster !== 'N/A' ? movie.Poster : 'https://placehold.jp/eeeeee/100x100.png?text=img ?'; // Placeholder image if poster is not available
+    listItem.innerHTML = `
+        <img src="${poster}" alt="${movie.Title}">
+        <div>
+        <h3>${movie.Title}</h3>
+        <p>${movie.Year}</p>
+        </div>
+    `;
+    movieList.appendChild(listItem);
+    });
+};
+
+const handleSearch = () => {
+    const query = document.getElementById('searchInput').value.trim();
+    if (query.length === 0) {
+    renderMovies([]);
+    return;
+    }
+    debounceSearch(async () => {
+    const movies = await searchMovies(query);
+    renderMovies(movies);
+    }, SEARCH_DELAY);
+};
+
+document.getElementById('searchInput').addEventListener('input', handleSearch);
+
+// Load more btn
+let currentPage = 1;
+    const resultsPerPage = 10;
+
+    const loadMoreMovies = async () => {
+      const query = document.getElementById('searchInput').value.trim();
+      if (query.length === 0) {
+        return;
+      }
+      try {
+        const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}&page=${currentPage}`);
+        const data = await response.json();
+        const movies = data.Search || [];
+        renderMovies(movies);
+        currentPage++;
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    };
+
+    document.getElementById('loadMoreBtn').addEventListener('click', loadMoreMovies);
+
