6d4b926038f19f83fcf578cbf3b469acc3d2e830
RISHAV YADAV <128891361+rishavy@users.noreply.github.com>
2024-04-15 00:58:18 +0530

    update script

--- Sticky Notes/script.js
+++ Sticky Notes/script.js
@@ -1,9 +1,10 @@
-const btn = document.getElementById("btn");
+const btn = document.getElementById("add-btn");
 btn.addEventListener("click", addNote);
+
 function addNote() {
-    let title = document.getElementById('noteTitle').value;
-    let content = document.getElementById('noteContent').value;
-    let color = document.getElementById('noteColor').value;
+    let title = document.getElementById('note-title').value;
+    let content = document.getElementById('note-text').value;
+    let color = document.getElementById('note-color').value;
 
     if (title.trim() === '' || content.trim() === '') {
         alert('Please enter both title and content for the note.');
@@ -11,19 +12,19 @@ function addNote() {
     }
 
     let note = document.createElement('div');
-    note.className = 'stickyNote';
+    note.className = 'sticky-note';
     note.style.backgroundColor = color;
     note.innerHTML = `
         <h2>${title}</h2>
         <textarea readonly>${content}</textarea>
-        <button onclick="editNote(this)">Edit</button>
-        <button onclick="deleteNote(this)">Delete</button>
+        <button class="btn-icon edit" onclick="editNote(this)">✎</button>
+        <button class="btn-icon close" onclick="deleteNote(this)">X</button>
     `;
 
     document.getElementById('container').appendChild(note);
 
-    document.getElementById('noteTitle').value = '';
-    document.getElementById('noteContent').value = '';
+    document.getElementById('note-title').value = '';
+    document.getElementById('note-text').value = '';
 }
 
 function editNote(btn) {
